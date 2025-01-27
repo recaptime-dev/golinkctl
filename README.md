0fc89bb483defbf7a6a08e4a0c0a8d66f03004e4
Andrei Jiroh Halili <ajhalili2006@andreijiroh.dev>
2025-01-28 07:54:29 +0800

    Imagine cooking this up :)

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# golinkctl
-TypeScript-based CLI for managing your https://github.com/tailscale/golink compatible API and friends, built in Deno.
+# `golinkctl`
+
+TypeScript-based CLI for managing your <https://github.com/tailscale/golink> compatible API and
+friends, built in Deno using [`jsr:@cliffy/command`](https://jsr.io/@cliffy/command) library
+instead of [`npm:commander`](https://npmjs.com/package/commander).
+
+## Install
+
+Requires Tailscale to be installed and authenicated with MagicDNS enabled
+if you want to use the default base URL.
+
+```bash
+deno install -gArf jsr:@recaptime-dev/golinkctl
+```
+
+## Usage
+
+* `golinkctl set http://reallylonglink.com your-mom` - create a golink with custom short link
+(omit `your-mom` to generate a 8-character slug)
+  * note that you can reuse the `set` command to update existing golinks
+  * to avoid confusion and to ease integration, command aliases `new` and `update` exist
+* `golinkctl info <short>` - get information about a golink
+* `golinkctl export -f ./bak` -  back up JSON Lines-formatted golinks DB
+* `golinkctl --url https://go.andreijiroh.dev --api-key ${GITHUB_TOKEN} ...` - example usage for @ajhalili2006's golink server
+
+### Environment variables / global flags
+
+* `GOLINK_URL` / `-u | --url` - defaults to `http://go`
+* `GOLINK_API_KEY` / `-k | --api-key` - golink API key
+
+## License
+
+MPL-2.0
---
+++ golinkctl.ts
@@ -0,0 +1,161 @@
+#!/usr/bin/env -S deno run -A
+import { Command } from "@cliffy/command";
+
+// types go here
+const headers: Record<string, string> = {
+  "Sec-Golink": "1",
+  "Authorization": ""
+}
+
+type GoLinkData = {
+  Short: string,
+  Long: string,
+  Created: string,
+  LastEdit: string,
+  Owner: string,
+  Clicks?: number
+}
+
+// utility function go here
+function generateSlug(length: number) {
+  let result = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+async function getBaseGolink(url: string) {
+  if (url == "http://go") {
+    const chaos = await fetch(url)
+    return chaos.url
+  } else {
+    return url
+  }
+}
+
+// the main entrypoint
+const command = await new Command()
+  .name("golinkctl")
+  .env(
+    "URL=<url:string>",
+    "base URL of the golink server, used if you manage another tailnet's golink server using shared machines or building your own over the internet",
+    { global: true, required: false, prefix: "GOLINK_", value: "http://go" }
+  )
+  .env(
+    "API_KEY=<apiKey:string>",
+    "Golink API key, used by custom implementations of golink server (see https://github.com/andreijiroh-dev/golinks for context)",
+    { global: true, required: false, prefix: "GOLINK_", value: "" }
+    )
+  .description("manage your golinks for https://github.com/tailscale-dev/golink compat API servers")
+  .globalOption("-u, --url [url:string]", "the base URL of your golink server", {
+    default: "http://go"
+  })
+  .globalOption("-k, --api-key [apiKey:string]", "API key for some custom golink server implementation")
+  .action((options, ...args) => console.log("Run this again with `--help` for hints."))
+
+command.command("set", "create a new golink (or update a existing one)")
+  .alias("new").alias("update")
+  .arguments("<target:string [golink:string]")
+  .action(async(options, ...args) => {
+    const long = args[0]
+    const short = args[1] || generateSlug(8)
+
+    if (options.apiKey) {
+      headers.Authorization = `bearer ${options.apiKey}`
+    }
+    headers["Content-Type"] = "application/x-www-form-urlencoded"
+
+    try {
+      const data = await fetch(await getBaseGolink(`${options.url}`), {
+        method: "POST",
+        body: new URLSearchParams({
+          long,
+          short
+        }),
+        headers,
+        redirect: "follow"
+      })
+
+      if (data.ok == true) {
+        const json: GoLinkData = await data.json()
+
+        const log =`\
+Short: ${json.Short}
+Long: ${json.Long}
+Created on: ${json.Created}
+Owner: ${json.Owner}
+Last edited: ${json.LastEdit}`
+        console.log(log)
+      }
+    } catch (error) {
+      throw Error(error)
+    }
+  })
+
+// info
+command.command("info", "show details about a golink")
+  .arguments("<golink:string>")
+  .action(async (options, args) => {
+    console.log(options, args)
+
+    if (options.apiKey) {
+      headers.Authorization = `bearer ${options.apiKey}`
+    }
+
+    try {
+      const data = await fetch(`${options.url}/${args}+`, {
+        headers,
+        redirect: "follow"
+      })
+
+      if (data.status === 404) {
+        console.error(`error: golink ${args} does not exist`)
+        Deno.exit(1)
+      }
+      const json: GoLinkData = await data.json()
+
+      // print the hell out
+      const log = `\
+Short: ${json.Short}
+Long: ${json.Long}
+Created on: ${json.Created}
+Owner: ${json.Owner}
+Last edited: ${json.LastEdit}
+Clicks/opens: ${json.Clicks || 0}`
+
+      console.log(log)
+    } catch (error) {
+      throw new Error(error);
+    }
+  })
+
+// export
+command.command("export", "export your golinks in JSON Lines format")
+  .option("-f, --file <file:file>", "path to output file for exports instead of via stdout")
+  .action(async (options) => {
+    if (options.apiKey) {
+      headers.Authorization = `bearer ${options.apiKey}`
+    }
+
+    try {
+      const data = await fetch(`${options.url}/.export`, {
+        headers,
+        redirect: "follow"
+      })
+      const text = await data.text()
+
+      if (options.file == null) {
+        console.log(text)
+      } else {
+        await Deno.writeTextFile(options.file, text)
+        console.log("Successfully written to "+options.file)
+      }
+    } catch (error) {
+      throw new Error(error)
+    }
+  })
+
+command.parse(Deno.args)
